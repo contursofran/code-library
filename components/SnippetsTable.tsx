6ad0c3ed3c6e04ae3937590d8ce2149249d6588d
fconturso <contursofran@gmail.com>
2023-05-21 01:06:56 -0300

    Fix #5, add table styles

--- components/SnippetsTable.tsx
+++ components/SnippetsTable.tsx
@@ -1,40 +1,60 @@
 "use client"
 
 import {
-  ColumnDef,
+  createColumnHelper,
   flexRender,
   getCoreRowModel,
   useReactTable,
 } from "@tanstack/react-table"
 
-import { formatDate } from "@/lib/utils"
+import { formatDate, upperFirst } from "@/lib/utils"
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table"
 
 import { Snippet } from "../types"
+import { Badge } from "./ui/badge"
 
 interface DataTableProps<TData> {
   data: TData[]
 }
 
-type SnippetColumnDef<TValue> = ColumnDef<Snippet, TValue>
+const columnHelper = createColumnHelper<Snippet>()
 
-export function DataTable<TData, TValue>({ data }: DataTableProps<TData>) {
-  const columns: SnippetColumnDef<TValue>[] = [
-    {
-      accessorKey: "content.title",
-    },
-    {
-      accessorKey: "content.language",
-    },
-    {
-      accessorKey: "tags",
-    },
-    {
-      accessorKey: "date",
-      cell: ({ row }) => {
-        return <div>{formatDate(row.getValue("date"))}</div>
-      },
-    },
+export function DataTable<TData>({ data }: DataTableProps<TData>) {
+  const columns = [
+    columnHelper.accessor("content", {
+      cell: (row) => (
+        <div className="flex flex-col space-y-1">
+          <div className="text-lg font-medium leading-none tracking-tight">
+            {upperFirst(row.getValue().title)}
+          </div>
+          <div className="text-sm text-gray-500">
+            {upperFirst(row.getValue().description)}
+          </div>
+        </div>
+      ),
+    }),
+    columnHelper.accessor("content.language", {
+      cell: (row) => (
+        <Badge className="text-xs" variant="outline">
+          <div className="p-1">{upperFirst(row.getValue())}</div>
+        </Badge>
+      ),
+    }),
+    columnHelper.accessor("tags", {
+      cell: (row) => (
+        <div className="flex flex-wrap items-center">
+          {row.getValue().map((tag) => (
+            <Badge className="mr-2 dark:bg-slate-50" key={tag}>
+              {upperFirst(tag)}
+            </Badge>
+          ))}
+        </div>
+      ),
+    }),
+    columnHelper.accessor("date", {
+      header: "Date",
+      cell: (row) => <div>{formatDate(row.getValue())}</div>,
+    }),
   ]
 
   const table = useReactTable({
