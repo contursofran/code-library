ab6abd01f32a8e62a3c39c0cc31d049ce392433c
fconturso <contursofran@gmail.com>
2023-06-11 18:11:45 -0300

    fix: tailwind colors warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 const { fontFamily } = require("tailwindcss/defaultTheme")
+
 const colors = require("tailwindcss/colors")
+
+delete colors["lightBlue"]
+delete colors["warmGray"]
+delete colors["trueGray"]
+delete colors["coolGray"]
+delete colors["blueGray"]
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
