96ce486d9ffb720247495e22ce4d7a43a8c0778f
fconturso <contursofran@gmail.com>
2023-06-13 17:09:52 -0300

    fix: next-auth session callback

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,18 @@
 import { env } from "@/env.mjs"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-import { NextAuthOptions } from "next-auth"
+import { db } from "@vercel/postgres"
+import { DefaultSession, NextAuthOptions } from "next-auth"
 import GitHubProvider from "next-auth/providers/github"
 
-const prisma = new PrismaClient()
+import prisma from "@/lib/prisma"
+
+// declare module "next-auth" {
+//   interface Session extends DefaultSession {
+//     user: DefaultSession["user"] & {
+//       id: string
+//     }
+//   }
+// }
 
 export const authOptions: NextAuthOptions = {
   session: {
@@ -20,4 +28,37 @@ export const authOptions: NextAuthOptions = {
       clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
+      }
+
+      return session
+    },
+    async jwt({ token, user }) {
+      const dbUser = await prisma.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      })
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id
+        }
+        return token
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      }
+    },
+  },
 }
