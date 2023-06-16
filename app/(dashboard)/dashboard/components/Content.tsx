7c9c96890eff79bc61c4a8999b891156fc72c250
fconturso <contursofran@gmail.com>
2023-06-16 12:42:06 -0300

    feat: get selected snippet

--- app/(dashboard)/dashboard/components/Content.tsx
+++ app/(dashboard)/dashboard/components/Content.tsx
@@ -16,15 +16,15 @@ function fixCode(code: string) {
 }
 
 export default function Content({ snippet }: SnippetContentProps) {
-  const code = fixCode(snippet.content.code)
+  const code = fixCode(snippet.code)
 
   return (
     <div className="container mt-8 flex h-full w-full flex-col gap-6">
       <div className="flex w-full items-center justify-between">
         <div className="flex flex-col gap-1">
-          <div className="text-2xl tracking-tight">{snippet.content.title}</div>
+          <div className="text-2xl tracking-tight">{snippet.title}</div>
           <div className="text-lg dark:text-gray-500">
-            {snippet.content.description}
+            {snippet.description}
           </div>
         </div>
         <Editor action="edit" snippet={snippet} />
@@ -32,11 +32,10 @@ export default function Content({ snippet }: SnippetContentProps) {
       <div className="flex w-full justify-center">
         <div className="w-3/5 rounded-lg border p-1">
           <div className="flex justify-between">
-            {/* @ts-expect-error */}
             <Code
               className="w-full text-sm"
               codeClassName="-my-2 mx-2"
-              lang={snippet.content.language}
+              lang={snippet.language}
               theme={{
                 dark: tokyoNightTheme,
                 light: "min-light",
--- app/(dashboard)/dashboard/snippets/[id]/page.tsx
+++ app/(dashboard)/dashboard/snippets/[id]/page.tsx
@@ -1,5 +1,8 @@
-import { snippets } from "@/data/data"
+import { redirect } from "next/navigation"
 
+import { authOptions } from "@/lib/auth"
+import prisma from "@/lib/prisma"
+import { getCurrentUser } from "@/lib/session"
 import Content from "@/app/(dashboard)/dashboard/components/Content"
 
 interface SnippetsPageProps {
@@ -8,14 +11,25 @@ interface SnippetsPageProps {
   }
 }
 
-async function getUserSnippet(id: string) {
-  const snippet = snippets.find((snippet) => snippet.id === id)
+async function getUserSnippet(postId: string, userId: string) {
+  const snippet = await prisma.snippet.findFirst({
+    where: {
+      id: postId,
+      authorId: userId,
+    },
+  })
 
   return snippet
 }
 
 export default async function SnippetsPage({ params }: SnippetsPageProps) {
-  const snippet = await getUserSnippet(params.id)
+  const user = await getCurrentUser()
+
+  if (!user) {
+    redirect(authOptions.pages?.signIn || "/login")
+  }
+
+  const snippet = await getUserSnippet(params.id, user.id)
 
   if (snippet) {
     return <Content snippet={snippet} />
