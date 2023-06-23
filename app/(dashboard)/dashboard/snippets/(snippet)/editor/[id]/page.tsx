ae8932251b5d0720fa369c01c0f821e02e9a205e
fconturso <contursofran@gmail.com>
2023-06-23 12:51:00 -0300

    refactor: reorganize folder structure

---
+++ app/(dashboard)/dashboard/snippets/(snippet)/editor/[id]/page.tsx
@@ -0,0 +1,11 @@
+import SnippetForm from "@/app/(dashboard)/dashboard/snippets/components/SnippetForm"
+
+export default async function NewSnippetPage() {
+  return (
+    <div className="flex-col items-center justify-center">
+      <div className="w-full flex-col justify-center">
+        <SnippetForm />
+      </div>
+    </div>
+  )
+}
