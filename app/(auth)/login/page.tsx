e230d1c1a7d4668b76c836b94fc88f807c1d7fd0
fconturso <contursofran@gmail.com>
2023-07-02 18:27:49 -0300

    fix: remove option to login with email for now

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -33,7 +33,7 @@ export default function LoginPage() {
             Welcome back
           </h1>
           <p className="text-sm text-muted-foreground">
-            Enter your email to sign in to your account
+            Please continue with your github account
           </p>
         </div>
         <UserAuthForm />
--- components/forms/UserAuthForm.tsx
+++ components/forms/UserAuthForm.tsx
@@ -38,44 +38,6 @@ export default function UserAuthForm({
 
   return (
     <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="grid gap-2">
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
-              Email
-            </Label>
-            <Input
-              disabled
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              id="email"
-              placeholder="name@example.com"
-              type="email"
-              {...register("email")}
-            />
-            {errors?.email && (
-              <p className="px-1 text-xs text-red-600">
-                {errors.email.message}
-              </p>
-            )}
-          </div>
-          <button disabled className={cn(buttonVariants())}>
-            {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-            Sign In with Email
-          </button>
-        </div>
-      </form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-      </div>
       <button
         className={cn(buttonVariants({ variant: "outline" }))}
         disabled={isLoading || isGitHubLoading}
