dbf079344bee32a9193a448cf4f48ae7db6eac50
fconturso <contursofran@gmail.com>
2023-07-10 19:11:16 -0300

    chore: add prettier config back in vscode 1.80

--- prettier.config.js
+++ prettier.config.js
@@ -1,23 +1,23 @@
 /** @type {import('prettier').Config} */
 
-// const tailwindPlugin = require("prettier-plugin-tailwindcss")
-// const importsPlugin = require("@ianvs/prettier-plugin-sort-imports")
+const tailwindPlugin = require("prettier-plugin-tailwindcss")
+const importsPlugin = require("@ianvs/prettier-plugin-sort-imports")
 
-// // disable tailwind plugin for now until vscode 1.80 is released https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/168
-// const plugins = [
-//   {
-//     parsers: {
-//       typescript: {
-//         ...tailwindPlugin.parsers.typescript,
-//         preprocess: importsPlugin.parsers.typescript.preprocess,
-//       },
-//     },
-//     options: {
-//       ...tailwindPlugin.options,
-//       ...importsPlugin.options,
-//     },
-//   },
-// ]
+// disable tailwind plugin for now until vscode 1.80 is released https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/168
+const plugins = [
+  {
+    parsers: {
+      typescript: {
+        ...tailwindPlugin.parsers.typescript,
+        preprocess: importsPlugin.parsers.typescript.preprocess,
+      },
+    },
+    options: {
+      ...tailwindPlugin.options,
+      ...importsPlugin.options,
+    },
+  },
+]
 
 module.exports = {
   endOfLine: "lf",
@@ -42,8 +42,7 @@ module.exports = {
     "",
     "^[./]",
   ],
-  // plugins,
-  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  plugins,
   importOrderSeparation: false,
   importOrderSortSpecifiers: true,
   importOrderBuiltinModulesToTop: true,
