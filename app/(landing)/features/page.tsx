d0f31b0b7e1980d477ba99b23b00d8c2f7722bc3
fconturso <contursofran@gmail.com>
2023-08-15 12:36:08 -0300

    feat: add marketing features

--- app/(landing)/features/page.tsx
+++ app/(landing)/features/page.tsx
@@ -1,21 +1,38 @@
-import { FeaturesCard } from "@/components/features-card"
-import { Icons } from "@/components/icons"
+import { siteFeatures } from "@/config/site"
+import { cn } from "@/lib/utils"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Features({}) {
   return (
     <section className="container flex h-full flex-col justify-start gap-16  pb-8 pt-6 sm:gap-6 md:pb-12  md:pt-10 lg:pb-24 lg:pt-16">
       <div className="flex-col space-y-4">
         <div className="mx-auto flex flex-col items-start gap-4 ">
-          <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter sm:text-4xl md:text-5xl">
+          <h1 className="text-4xl font-bold leading-[1.1] tracking-tighter md:text-3xl lg:text-4xl xl:text-5xl">
             Features
           </h1>
-          <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+          <p className="text-xl leading-normal text-muted-foreground sm:leading-8 md:text-lg xl:text-xl">
             The goal of this project is to create a modern web app using Next.js
             13. These are some of the features that are planned.
           </p>
         </div>
       </div>
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 "></div>
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
+        {siteFeatures.map((feature) => (
+          <Card className={cn("p-2")} key={feature.title}>
+            <CardHeader>{feature.icon}</CardHeader>
+            <CardContent className="space-y-2">
+              <CardTitle className="text-lg">{feature.title}</CardTitle>
+              <CardDescription className="mt-2">{feature.body}</CardDescription>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </section>
   )
 }
--- config/site.tsx
+++ config/site.tsx
@@ -41,8 +41,86 @@ export const navItems: NavItem[] = [
 
 export const siteFeatures: siteFeature[] = [
   {
-    icon: <Icons.bell />,
+    icon: <Icons.nextjs className="h-10 w-10" />,
     title: "Next.js 13",
     body: <>Latest features from Next 13 using the brand new App Router.</>,
+  },
+  {
+    icon: <Icons.react className="h-10 w-10" />,
+    title: "React 18",
+    body: <>Server and Client Components.</>,
+  },
+  {
+    icon: <Icons.shadcn className="h-10 w-10" />,
+    title: "Components",
+    body: (
+      <>
+        Ui components from{" "}
+        <a
+          className="font-medium underline underline-offset-4"
+          href="https://ui.shadcn.com/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          shadcn
+        </a>
+        .
+      </>
+    ),
+  },
+  {
+    icon: <Icons.nextAuth className="h-10 w-10" />,
+    title: "Authentication",
+    body: (
+      <>
+        Authentication using{" "}
+        <a
+          className="font-medium underline underline-offset-4"
+          href="https://next-auth.js.org/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          NextAuth.js
+        </a>{" "}
+        and middlewares.
+      </>
+    ),
+  },
+  {
+    icon: (
+      <div className="flex gap-3 self-start">
+        <Icons.trpc className="h-10 w-10" />
+        <Icons.prisma className="h-10 w-10" />
+      </div>
+    ),
+    title: "Full-stack Typesafety",
+    body: (
+      <>
+        Full-stack Typesafety with{" "}
+        <a
+          className="font-medium underline underline-offset-4"
+          href="https://ui.shadcn.com/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          tRPC
+        </a>
+        . ORM using{" "}
+        <a
+          className="font-medium underline underline-offset-4"
+          href="https://ui.shadcn.com/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Prisma
+        </a>
+        .
+      </>
+    ),
+  },
+  {
+    icon: <Icons.vercel className="h-10 w-10" />,
+    title: "Vercel Postgres",
+    body: <>Database using Vercel Postgres.</>,
   },
 ]
