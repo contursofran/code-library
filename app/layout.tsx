d91630aff458820325886d171a9367d05e5ff82a
fconturso <contursofran@gmail.com>
2023-08-22 13:39:25 -0300

    feat: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import { Inter as FontSans } from "next/font/google"
 
 import "@/styles/globals.css"
 
+import { Metadata } from "next"
+
+import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -13,6 +16,17 @@ const fontSans = FontSans({
 
 interface RootLayoutProps {
   children: React.ReactNode
+}
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: "%s | " + siteConfig.name,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
