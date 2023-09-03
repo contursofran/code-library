3e292e4647ccf269d2caeef12a7ea1cd34a9b50c
fconturso <contursofran@gmail.com>
2023-09-02 23:45:55 -0300

    feat: fetch snippets using trpc

--- app/(dashboard)/dashboard/snippets/(snippets)/page.tsx
+++ app/(dashboard)/dashboard/snippets/(snippets)/page.tsx
@@ -2,16 +2,13 @@ import Link from "next/link"
 import { redirect } from "next/navigation"
 
 import { authOptions } from "@/lib/auth"
-import prisma from "@/lib/prisma"
 import { getCurrentUser } from "@/lib/session"
 import { Button } from "@/components/ui/button"
 import { DataTable } from "@/components/dashboard/data-table"
 import { columns } from "@/components/dashboard/data-table-columns"
 import { Header } from "@/components/header"
 import { Shell } from "@/components/shell"
-
-export const dynamic = "force-dynamic"
-export const fetchCache = "force-no-store"
+import { serverClient } from "@/app/_trpc/server-client"
 
 async function getSnippets() {
   const user = await getCurrentUser()
@@ -20,19 +17,7 @@ async function getSnippets() {
     redirect(authOptions.pages?.signIn || "login")
   }
 
-  const snippets = await prisma.snippet.findMany({
-    select: {
-      id: true,
-      title: true,
-      code: true,
-      description: true,
-      date: true,
-      language: true,
-    },
-    where: {
-      authorId: user.id,
-    },
-  })
+  const snippets = await serverClient.getSnippets({ id: user.id })
 
   return snippets
 }
--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,43 @@
+import { redirect } from "next/navigation"
 import { publicProcedure, router } from "@/server/trpc"
+import { z } from "zod"
 
-export const appRouter = router({})
+import { authOptions } from "@/lib/auth"
+import prisma from "@/lib/prisma"
+import { getCurrentUser } from "@/lib/session"
+
+export const appRouter = router({
+  getSnippets: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query(async (opts) => {
+      const { id } = opts.input
+      const snippets = await prisma.snippet.findMany({
+        select: {
+          id: true,
+          title: true,
+          code: true,
+          description: true,
+          date: true,
+          language: true,
+        },
+        where: {
+          authorId: id,
+        },
+      })
+
+      return snippets
+    }),
+  getSnippet: publicProcedure.query(async (opts) => {
+    const asd = {
+      id: "test",
+    }
+
+    return asd
+  }),
+})
 
 export type AppRouter = typeof appRouter
